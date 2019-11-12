ddf0dcb21b2596bf7353c57e7540589781879646
Yuichiro Nishikawa <ryuujin8935648@yahoo.co.jp>
2019-11-12 18:05:32 +0900

    Add files via upload

---
+++ Special_Programming/p94_2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def cross_entropy_error(y,t):
+    if y.ndim==1:
+        t=t.reshape(1,t.size)
+        y=y.reshape(1,y.size)
+    
+    batch_size=y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size),t]+1e-7))/batch_size
+
+t=[2,1]
+y=[.1,.05,.6,.0,.05,.1,.0,.1,.0,.0]
+print(cross_entropy_error(np.array(y),np.array(t)))
+
+y=[.1,.05,.1,.0,.05,.1,.0,.6,.0,.0]
+print(cross_entropy_error(np.array(y),np.array(t)))
