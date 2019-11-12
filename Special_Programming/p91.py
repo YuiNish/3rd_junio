554d674beaa3376f3eeed4fdf8410a6275c236ae
Yuichiro Nishikawa <ryuujin8935648@yahoo.co.jp>
2019-11-12 11:57:01 +0900

    Add files via upload

---
+++ Special_Programming/p91.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def mean_entropy_error(y,t):
+    delta=1e-7
+    return -np.sum(t*np.log(y+delta))
+
+t=[0,0,1,0,0,0,0,0,0,0]
+y=[.1,.05,.6,.0,.05,.1,.0,.1,.0,.0]
+print(mean_entropy_error(np.array(y),np.array(t)))
+
+y=[.1,.05,.1,.0,.05,.1,.0,.6,.0,.0]
+print(mean_entropy_error(np.array(y),np.array(t)))
